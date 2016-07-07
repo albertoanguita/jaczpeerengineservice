4a15a7f731cd9b40f6572b57cd9005159b52fb69
alberto <alberto.anguita@gmail.com>
2016-07-07 14:03:48 +0200

    Change version constant

--- src/main/java/jacz/peerengineservice/client/PeerClient.java
+++ src/main/java/jacz/peerengineservice/client/PeerClient.java
@@ -41,7 +41,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
  */
 public class PeerClient {
 
-    public static final String VERSION = "0.1.0";
+    public static final String API_VERSION = "0.1.0";
 
     public static final String OWN_CUSTOM_PREFIX = "@@@";
 
